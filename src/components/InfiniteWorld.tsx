34f097eaf8563b9b7e8ca5c2582b8f851ee6277f
tmba <ut42.nu@gmail.com>
2025-11-03 23:36:46 +0900

    feat: implement InfiniteWorld component and integrate with Scene

---
+++ src/components/InfiniteWorld.tsx
@@ -0,0 +1,56 @@
+import { Text } from "@react-three/drei";
+import { BvhPhysicsBody, PrototypeBox } from "@react-three/viverse";
+import {
+  GRID_CELL_SIZE,
+  HALF_GRID_CELL_SIZE,
+  useWorld,
+} from "@/hooks/useWorld";
+
+export const InfiniteWorld = ({ ...props }) => {
+  const gridPosition = useWorld((state) => state.gridPosition);
+  const gridSize = useWorld((state) => state.gridSize);
+  return (
+    <group
+      {...props}
+      position-x={(-gridSize.x / 2) * GRID_CELL_SIZE + HALF_GRID_CELL_SIZE} // Center the grid
+      position-y={-0.5}
+      position-z={(-gridSize.y / 2) * GRID_CELL_SIZE + HALF_GRID_CELL_SIZE} // Center the grid
+    >
+      {Array.from({ length: gridSize.x }, (_, i) => i).map((x) =>
+        Array.from({ length: gridSize.y }, (_, j) => j).map((y) => {
+          const cellX = x + gridPosition.x - Math.floor(gridSize.x / 2);
+          const cellY = y + gridPosition.y - Math.floor(gridSize.y / 2);
+          return (
+            <group
+              key={`${x}-${y}`}
+              position={[
+                x * GRID_CELL_SIZE + gridPosition.x * GRID_CELL_SIZE,
+                0,
+                y * GRID_CELL_SIZE + gridPosition.y * GRID_CELL_SIZE,
+              ]}
+            >
+              <BvhPhysicsBody kinematic>
+                <PrototypeBox
+                  scale={[GRID_CELL_SIZE, 1, GRID_CELL_SIZE]}
+                  position={[0, -2, 0]}
+                />
+              </BvhPhysicsBody>
+              <Text
+                fontSize={1}
+                position-y={-1.49}
+                rotation-x={-Math.PI / 2}
+                fontWeight={"bold"}
+                textAlign="center"
+                lineHeight={1}
+                receiveShadow
+              >
+                CELL{"\n"}[{cellX}, {cellY}]
+                <meshStandardMaterial color="white" />
+              </Text>
+            </group>
+          );
+        }),
+      )}
+    </group>
+  );
+};
--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,26 +1,69 @@
 import { Sky } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
 import {
-  BvhPhysicsBody,
-  PrototypeBox,
   SimpleCharacter,
+  type SimpleCharacterImpl,
 } from "@react-three/viverse";
-import { Suspense } from "react";
+import { Suspense, useEffect, useRef } from "react";
+import { type DirectionalLight, Vector3 } from "three";
+import { InfiniteWorld } from "@/components/InfiniteWorld";
+import { useWorld } from "@/hooks/useWorld";
 
-export function Scene() {
+const LIGHT_OFFSET = new Vector3(2, 5, 2);
+const tmpVec = new Vector3();
+
+export const Scene = () => {
+  const computeCharacterCell = useWorld((state) => state.computeCharacterCell);
+  const characterRef = useRef<SimpleCharacterImpl>(null);
+  const directionalLight = useRef<DirectionalLight | null>(null);
+  const { scene } = useThree();
+
+  useEffect(() => {
+    const light = directionalLight.current;
+    if (!light) {
+      return;
+    }
+
+    scene.add(light.target);
+    return () => {
+      scene.remove(light.target);
+    };
+  }, [scene]);
+
+  useFrame(() => {
+    const character = characterRef.current;
+    const light = directionalLight.current;
+
+    if (!character || !light) {
+      return;
+    }
+
+    computeCharacterCell(character.position);
+
+    // Keep the light aligned with the character so shadows stay accurate
+    light.target.position.copy(character.position);
+    tmpVec.copy(light.target.position).add(LIGHT_OFFSET);
+    light.position.copy(tmpVec);
+  });
+
   return (
     <>
       <Sky />
-
-      <directionalLight intensity={1.2} position={[5, 10, 10]} castShadow />
+      <directionalLight
+        intensity={1.2}
+        position={[-10, 10, -10]}
+        castShadow
+        ref={directionalLight}
+      />
       <ambientLight intensity={1} />
 
-      <Suspense fallback={null}>
-        <SimpleCharacter />
+      <Suspense>
+        <SimpleCharacter ref={characterRef} />
       </Suspense>
 
-      <BvhPhysicsBody>
-        <PrototypeBox scale={[10, 1, 10]} position={[0, -2, 0]} />
-      </BvhPhysicsBody>
+      <Suspense>
+        <InfiniteWorld />
+      </Suspense>
     </>
   );
-}
+};
