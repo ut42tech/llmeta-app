2dab1122529beffe118fb06689695ded601e43b9
tmba <ut42.nu@gmail.com>
2025-11-20 22:21:33 +0900

    feat: add ChatWindow component for text chat functionality

--- src/components/overlay/OverlayUI.tsx
+++ src/components/overlay/OverlayUI.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Dock } from "@/components/overlay/Dock";
+import { ChatWindow } from "@/components/overlay/dock-action/ChatWindow";
 import { StatusBar } from "@/components/overlay/StatusBar";
 
 export const OverlayUI = () => {
@@ -8,6 +9,7 @@ export const OverlayUI = () => {
     <div className="fixed inset-0 z-50 pointer-events-none">
       <StatusBar />
       <Dock />
+      <ChatWindow />
     </div>
   );
 };
